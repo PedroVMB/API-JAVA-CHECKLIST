1fcbc2ed1ec82b2c23595ec77cc7b62e86ac2222
PedroVMB <pedromb.dev@gmail.com>
2024-04-27 14:24:04 -0300

    fix: corrigindo erro de criacao da tabela de usuarios

--- src/main/java/com/br/APIChecklist/domain/condominio/DadosDetalhamentoCondominio.java
+++ src/main/java/com/br/APIChecklist/domain/condominio/DadosDetalhamentoCondominio.java
@@ -4,6 +4,6 @@ import com.br.APIChecklist.domain.endereco.Endereco;
 
 public record DadosDetalhamentoCondominio(Long id, String nome, String cnpj, int quantidade_torres, Endereco endereco, Boolean ativo) {
     public DadosDetalhamentoCondominio(Condominio condominio){
-        this(condominio.getId(), condominio.getNome(), condominio.getCnpj(), condominio.getQuantiadade_torres(), condominio.getEndereco(), condominio.getAtivo());
+        this(condominio.getId(), condominio.getNome(), condominio.getCnpj(), condominio.getQuantidade_torres(), condominio.getEndereco(), condominio.getAtivo());
     }
 }
--- src/main/java/com/br/APIChecklist/domain/condominio/DadosListagemCondominio.java
+++ src/main/java/com/br/APIChecklist/domain/condominio/DadosListagemCondominio.java
@@ -2,6 +2,6 @@ package com.br.APIChecklist.domain.condominio;
 
 public record DadosListagemCondominio (Long id, String nome, String cnpj, int quantidade_torres){
     public DadosListagemCondominio(Condominio condominio){
-        this(condominio.getId(), condominio.getNome(), condominio.getCnpj(), condominio.getQuantiadade_torres());
+        this(condominio.getId(), condominio.getNome(), condominio.getCnpj(), condominio.getQuantidade_torres());
     }
 }
