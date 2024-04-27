a77ec684369f02d66dd8a2333d80168fae68927f
PedroVMB <pedromb.dev@gmail.com>
2024-04-27 13:40:31 -0300

    feat: adicionando os arquivos de migration para migracao no banco de dados e adicionando as pastas para desenvolvimento

---
+++ src/main/java/com/br/APIChecklist/ApiChecklistApplication.java
@@ -0,0 +1,13 @@
+package com.br.APIChecklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChecklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChecklistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/APIChecklist/ApiChecklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.APIChecklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiChecklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
